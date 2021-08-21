6c922734f2a64d58c3dacc03387fb6edc6e67aa6
Med Aduh <im.ramidem@gmail.com>
2021-08-22 00:07:47 +0800

    [setup] now lerna is working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import User from '@rangk/common/src/User';
 
 const user: User = {
-
+  firstName: 'Marco',
+  lastName: 'Polo',
 }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Hello />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, {user.firstName}</h1>
     </div>
   );
 }
