2dd17d73721083d3b803957a5f79a1e7d603077a
Med Aduh <im.ramidem@gmail.com>
2021-08-22 04:10:30 +0800

    [security] helmet

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from 'express';
+import helmet from 'helmet';
 
 const app = express();
+
+app.use(helmet());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!\n');
