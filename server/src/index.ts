d4f0bea6863e40aa0f60b4a4258ba805bdf372f1
Med Aduh <im.ramidem@gmail.com>
2021-08-22 16:57:37 +0800

    [setup] lint common

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
 import helmet from 'helmet';
 
+import { apples } from '@rangk/common/src/User';
+
+const mansanas = apples;
+
 const app = express();
 
 app.use(helmet());
