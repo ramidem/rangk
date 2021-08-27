460f1e67dd10d47998b6efc2a571f6cc65e50720
Med Aduh <im.ramidem@gmail.com>
2021-08-28 02:13:03 +0800

    [setup] jest

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,7 +2,6 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders a greeting', () => {
-  render(<App />);
   const user = screen.getByText(/hello/gi);
   expect(user).toBeInTheDocument();
 });
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,7 @@
-import './App.css';
-
-import User from '@rangk/common/src/User';
-
-const user: User = {
-  firstName: 'Marco',
-  lastName: 'Polo',
-}
-
 function App() {
   return (
     <div className="App">
-      <h1>Hello, {user.firstName}</h1>
+      <h1>Hello!</h1>
     </div>
   );
 }
